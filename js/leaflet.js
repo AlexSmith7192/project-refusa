223409073710f966b894f3863d64eed485153166
Alex Smith <alexsmith7192@gmail.com>
2020-09-01 21:32:30 -0300

    Editando mensagem dos POPUPs

--- js/leaflet.js
+++ js/leaflet.js
@@ -34,157 +34,157 @@ var municipios_piaui = L.geoJSON(municipios_piaui, {
 var altolonga_bomlugar = L.geoJSON(altolonga_bomlugar, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Área: <br> Perímetro: <br> Quantidade de moradores: <br> Território de Desenvolvimento: Entre Rios")
+}).addTo(map).bindPopup("Assentamento Bom Lugar<br>Área: 141,9417 Ha<br>Perímetro: 5657,4218 m<br>Quantidade de famílias: 09<br>Município: Alto Longá<br>Território de Desenvolvimento: Entre Rios")
 
 var altolonga_marinopolis = L.geoJSON(altolonga_marinopolis, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
-
+}).addTo(map).bindPopup("Assentamento Marinópolis<br>Área: 1192,3550 Ha<br>Perímetro: 14327,0471 m<br>Quantidade de famílias: 30<br>Município: Alto Longá<br>Território de Desenvolvimento: Entre Rios")
+    
 var altos_bispado = L.geoJSON(altos_bispado, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Bispado<br>Área: 717,1448 Ha<br>Perímetro: 11453,0105 m<br>Quantidade de famílias: 13<br>Município: Altos<br>Território de Desenvolvimento: Entre Rios")
 
 var altos_malhadadaspedras = L.geoJSON(altos_malhadadaspedras, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Malhada das Pedras<br>Área: 211,5521 Ha<br>Perímetro: 12580,471 m<br>Quantidade de famílias: 15<br>Município: Altos<br>Território de Desenvolvimento: Entre Rios")
 
 var amarante_vivafeliz = L.geoJSON(amarante_vivafeliz, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Viva Feliz<br>Área: 442,8828 Ha<br>Perímetro: 9442,2107 m<br>Quantidade de famílias: 25<br>Município: Amarante<br>Território de Desenvolvimento: Entre Rios")
 
 var angical_chapadadohamilton = L.geoJSON(angical_chapadadohamilton, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Chapada do Hamilton<br>Área: 664,6647 Ha<br>Perímetro: 10988,2124 m<br>Quantidade de famílias: 19<br>Município: Angical do Piauí<br>Território de Desenvolvimento: Entre Rios")
 
 var aroazes_barrovermelho = L.geoJSON(aroazes_barrovermelho, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Barro Vermelho<br>Área: 399,9886 Ha<br>Perímetro: 8787,2631 m<br>Quantidade de famílias: 15<br>Município: Aroazes<br>Território de Desenvolvimento: Vale do Sambito")
 
 var batalha_maganos = L.geoJSON(batalha_maganos, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Maganos<br>Área: 264,9312 Ha<br>Perímetro: 7369,6526 m<br>Quantidade de famílias: 12<br>Município: Batalha<br>Território de Desenvolvimento: Cocais")
 
 var beneditinos_buritizinho = L.geoJSON(beneditinos_buritizinho, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Buritizinho<br>Área: 427,3041 Ha<br>Perímetro: 9133,4969 m<br>Quantidade de famílias: 20<br>Município: Beneditinos<br>Território de Desenvolvimento: Entre Rios")
 
 var buritidosmontes_canabrava = L.geoJSON(buritidosmontes_canabrava, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Cana Brava<br>Área: 972,0705 Ha<br>Perímetro: 16861,4499 m<br>Quantidade de famílias: 20<br>Município: Buriti dos Montes<br>Território de Desenvolvimento: Carnaubais")
 
 var campomaior_tangara = L.geoJSON(campomaior_tangara, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Tangará<br>Área: 789,1977 Ha<br>Perímetro: 12780,6059 m<br>Quantidade de famílias: 25<br>Município: Campo Maior<br>Território de Desenvolvimento: Carnaubais")
 
 var elesbaoveloso_caraibas = L.geoJSON(elesbaoveloso_caraibas, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Caraíbas<br>Área: 614,0244 Ha<br>Perímetro: 12115,4115 m<br>Quantidade de famílias: 16<br>Município: Elesbão Veloso<br>Território de Desenvolvimento: Vale do Sambito")
 
 var flores_tamboril = L.geoJSON(flores_tamboril, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Tamboril<br>Área: 639,5015 Ha<br>Perímetro: 13514,9164 m<br>Quantidade de famílias: 20<br>Município: Flores do Piauí<br>Território de Desenvolvimento: Vale dos Rios Piauí e Itaueira")
 
 var floriano_caneladevelho = L.geoJSON(floriano_caneladevelho, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Canela de Velho<br>Área: 555,0978 Ha<br>Perímetro: 10777,9780 m<br>Quantidade de famílias: 23<br>Município: Floriano<br>Território de Desenvolvimento: Vale dos Rios Piauí e Itaueira")
 
 var geminiano_cacimbinha = L.geoJSON(geminiano_cacimbinha, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Cacimbinha<br>Área: 581,1940 Ha<br>Perímetro: 13502,1172 m<br>Quantidade de famílias: 27<br>Município: Geminiano<br>Território de Desenvolvimento: Vale do Rio Guaribas")
 
 var jardimdomulato_lagoadoscocos = L.geoJSON(jardimdomulato_lagoadoscocos, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Lagoa dos Cocos<br>Área: 130,9703 Ha<br>Perímetro: 9180,6615 m<br>Quantidade de famílias: 18<br>Município: Jardim do Mulato<br>Território de Desenvolvimento: Entre Rios")
 
 var josedefreitas_lembrada = L.geoJSON(josedefreitas_lembrada, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Lembrada<br>Área: 1013,6538 Ha<br>Perímetro: 13029,9086 m<br>Quantidade de famílias: 30<br>Município: José de Freitas<br>Território de Desenvolvimento: Entre Rios")
 
 var lagoadopiaui_espiritosanto = L.geoJSON(lagoadopiaui_espiritosanto, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Espírito Santo<br>Área: 330,8044 Ha<br>Perímetro: 9468,8485 m<br>Quantidade de famílias: 12<br>Município: Lagoa do Piauí<br>Território de Desenvolvimento: Entre Rios")
 
 var novooriente_maedagua = L.geoJSON(novooriente_maedagua, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Mãe D'Água<br>Área: 311,1109 Ha<br>Perímetro: 8461,7694 m<br>Quantidade de famílias: 13<br>Município: Novo Oriente<br>Território de Desenvolvimento: Vale do Sambito")
 
 var patos_palma = L.geoJSON(patos_palma, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Palma<br>Área: 913,1994 Ha<br>Perímetro: 15490,3064 m<br>Quantidade de famílias: 29<br>Município: Patos<br>Território de Desenvolvimento: Chapada Vale do Rio Itaim")
 
 var pedroii_arara = L.geoJSON(pedroii_arara, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Arara<br>Área: 669,3428 Ha<br>Perímetro: 12253,3802 m<br>Quantidade de famílias: 25<br>Município: Pedri II<br>Território de Desenvolvimento: Cocais")
 
 var pedroii_pedrabranca = L.geoJSON(pedroii_pedrabranca, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Pedra Branca<br>Área: 501,1361 Ha<br>Perímetro: 11098,8375 m<br>Quantidade de famílias: 20<br>Município: Pedro II<br>Território de Desenvolvimento: Cocais")
 
 var piripiri_cachoeira = L.geoJSON(piripiri_cachoeira, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Cachoeira<br>Área: 368,9189 Ha<br>Perímetro: 10560,0348 m<br>Quantidade de famílias: 15<br>Município: Piripiri<br>Território de Desenvolvimento: Cocais")
 
 var piripiri_sabia = L.geoJSON(piripiri_sabia, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Sabiá<br>Área: 181,7974 Ha<br>Perímetro: 6145,0488 m<br>Quantidade de famílias: 10<br>Município: Piripiri<br>Território de Desenvolvimento: Cocais")
 
 var santacruzdopiaui_vilanova = L.geoJSON(santacruzdopiaui_vilanova, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Vila Nova<br>Área: 845,9289 Ha<br>Perímetro: 16161,6352 m<br>Quantidade de famílias: 25<br>Município: Santa Crus do Piauí<br>Território de Desenvolvimento: Vale do Canindé")
 
 var saomigueldabaixagrande_mcruz = L.geoJSON(saomigueldabaixagrande_mcruz, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento M. Cruz<br>Área: 336,3634 Ha<br>Perímetro: 11357,5705 m<br>Quantidade de famílias: 20<br>Município: São Miguel da Baixa Grande<br>Território de Desenvolvimento: Vale do Sambito")
 
 var saopedro_caraibas = L.geoJSON(saopedro_caraibas, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Caraíbas<br>Área: 363,0171 Ha<br>Perímetro: 8292,9710 m<br>Quantidade de famílias: 18<br>Município: São Pedro do Piauí<br>Território de Desenvolvimento: Entre Rios")
 
 var saopedro_todosossantos = L.geoJSON(saopedro_todosossantos, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Todos os Santos<br>Área: 141,2048 Ha<br>Perímetro: 5412,3920 m<br>Quantidade de famílias: 10<br>Município: São Pedro do Piauí<br>Território de Desenvolvimento: Entre Rios")
 
 var teresina_bomsossego = L.geoJSON(teresina_bomsossego, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Bom Sossego<br>Área: 311,9144 Ha<br>Perímetro: 7856,0311 m<br>Quantidade de famílias: 25<br>Município: Teresina<br>Território de Desenvolvimento: Entre Rios")
 
 var teresina_caitetus = L.geoJSON(teresina_caitetus, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Caitetús<br>Área: 856,0573 Ha<br>Perímetro: 19715,0245 m<br>Quantidade de famílias: 20<br>Município: Teresina<br>Território de Desenvolvimento: Entre Rios")
 
 var valenca_santamaria = L.geoJSON(valenca_santamaria, {
     color: 'red',
     opacity: 1,
-}).addTo(map).bindPopup("Testando popup")
+}).addTo(map).bindPopup("Assentamento Santa Maria<br>Área: 672,2017 Ha<br>Perímetro: 7856,0311 m<br>Quantidade de famílias: 17<br>Município: Valença<br>Território de Desenvolvimento: Entre Rios")
 
 //grupo de camadas bases
 var baseMaps = [
@@ -198,7 +198,7 @@ var baseMaps = [
     }
 ];
 
-//grupo de camadas com as divisões politicas
+//grupo de camadas com as divisões politicas e os assentamentos
 var overlayers = [
     {
         groupName: "Camadas",
